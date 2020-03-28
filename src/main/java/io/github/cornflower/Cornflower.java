19568f7be5272077944210e43ff4b44f463b4b63
Joe van der Zwet <me@joezwet.me>
2020-03-28 13:35:44 +1300

    Initial Setup

---
+++ src/main/java/io/github/cornflower/Cornflower.java
@@ -0,0 +1,23 @@
+/*
+ * This file is part of Cornflower
+ * Copyright (C) 2020, Team Cornflower.
+ *
+ * This code is licensed under GNU General Public License v3.0, the full license text can be found in LICENSE
+ */
+
+package io.github.cornflower;
+
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Cornflower implements ModInitializer {
+
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/io/github/cornflower/client/CornflowerClient.java
@@ -0,0 +1,18 @@
+/*
+ * This file is part of Cornflower
+ * Copyright (C) 2020, Team Cornflower.
+ *
+ * This code is licensed under GNU General Public License v3.0, the full license text can be found in LICENSE
+ */
+
+package io.github.cornflower.client;
+
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CornflowerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
