8c86175cb8347fbfb92585aad976c4b37a695e53
Walker Knapp <walkerknapp3@gmail.com>
2020-04-02 21:18:52 -0400

    Removed errant sun.jvm import Does not exist on some JVMs

--- src/main/java/io/github/cornflower/item/CornflowerWand.java
+++ src/main/java/io/github/cornflower/item/CornflowerWand.java
@@ -11,7 +11,6 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import sun.jvm.hotspot.opto.Block;
 
 import java.util.Objects;
 
