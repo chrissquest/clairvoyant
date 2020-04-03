0990c02509eb70c0f63a2348ce196abf953b9128
Walker Knapp <walkerknapp3@gmail.com>
2020-04-03 00:36:44 -0400

    Resolved incorrect registration

--- src/main/java/io/github/cornflower/item/CornflowerItems.java
+++ src/main/java/io/github/cornflower/item/CornflowerItems.java
@@ -6,15 +6,13 @@ import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-import static io.github.cornflower.group.CornflowerGroup.CORNFLOWER_GROUP;
-
 public class CornflowerItems {
 
     public static final Item CORNFLOWER_WAND = registerItem(new CornflowerWand(), "wand_cornflower");
 
 
     private static Item registerItem(Item item, String id) {
-        return Registry.register(Registry.ITEM, new Identifier(ModConstants.MOD_ID, id), new Item(new Item.Settings().group(CORNFLOWER_GROUP)));
+        return Registry.register(Registry.ITEM, new Identifier(ModConstants.MOD_ID, id), new CornflowerWand());
     }
 
     public static void init() {}
