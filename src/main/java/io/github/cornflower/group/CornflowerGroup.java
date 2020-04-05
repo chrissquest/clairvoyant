fff1dd6fd0fa0d944f6acb52122858a46b139866
Chris <drago15243@gmail.com>
2020-04-05 12:29:41 -0500

    Cornflower group fix

--- src/main/java/io/github/cornflower/group/CornflowerGroup.java
+++ src/main/java/io/github/cornflower/group/CornflowerGroup.java
@@ -2,14 +2,14 @@ package io.github.cornflower.group;
 
 import io.github.cornflower.util.ModConstants;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 public class CornflowerGroup {
 
-    public static final ItemGroup CORNFLOWER_GROUP = FabricItemGroupBuilder.create(new Identifier(ModConstants.MOD_ID, "cornflower_group"))
-            .icon(Items.CORNFLOWER::getStackForRender).build();
+    public static final ItemGroup CORNFLOWER_GROUP = FabricItemGroupBuilder.create(new Identifier(ModConstants.MOD_ID, "cornflower_group")).icon(() -> new ItemStack(Blocks.CORNFLOWER)).build();
 
     public static void init() {}
 }
