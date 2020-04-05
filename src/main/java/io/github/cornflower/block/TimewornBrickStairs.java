f67cdb0195892193fdf422ecfc5cd95256f8cd82
BuildTools <unconfigured@null.spigotmc.org>
2020-04-05 12:38:34 -0500

    auto add Copyright maybe?

--- src/main/java/io/github/cornflower/block/TimewornBrickStairs.java
+++ src/main/java/io/github/cornflower/block/TimewornBrickStairs.java
@@ -1,3 +1,11 @@
+/*
+ * This file is part of Cornflower
+ * Copyright (C) 2020, Team Cornflower.
+ *
+ * This code is licensed under GNU General Public License v3.0, the full license text can be found in LICENSE
+ *
+ */
+
 package io.github.cornflower.block;
 
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
